a69acc58fc3d739da47be911f1c4181ab0b68cc8
HannaHorbenko <annasgorbenko@gmail.com>
2021-01-18 19:40:56 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        fade: true,
+        speed: 1000,
+        arrows: true
+    });
+});
+
+$('[data-fancybox="gallery"]').fancybox({
+	// Options will go here
+});
