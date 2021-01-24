3d2ecbef8586d444732f89bba2266afaf6b1839d
HannaHorbenko <annasgorbenko@gmail.com>
2021-01-24 19:37:09 +0200

    added animate

--- js/scripts.js
+++ js/scripts.js
@@ -56,3 +56,41 @@ function initMap() {
     infowindow.open(map, marker);
   });
 };
+function isInViewport(elem) {
+  let bounding = elem.getBoundingClientRect();
+  return (
+      bounding.top >= 0 &&
+      bounding.left >= 0 &&
+      bounding.top + bounding.height / 2 <= (window.innerHeight || document.documentElement.clientHeight) &&
+      bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
+function checkItems() {
+  let items = document.querySelectorAll('.animate');
+  
+  items.forEach(item => {
+    if(isInViewport(item)) {
+      let animateMode = item.getAttribute('data-animate'),
+          delay       = item.getAttribute('data-delay'),
+          speed       = item.getAttribute('data-speed');
+      item.classList.add('animate__animated', 'animate__zoomIn');
+
+      if (animateMode) {
+        item.classList.add('animate__animated', 'animate__' + animateMode);
+        
+      }
+      if (delay) {
+        item.classList.add(`animate__delay-${delay}s`);
+      }
+
+      if (speed) {
+        item.classList.add('animate__animated', 'animate__'  + speed);
+      }
+      item.classList.remove('animate');
+    }
+  }) 
+}
+checkItems();
+window.addEventListener('scroll', function(e) {
+  checkItems();
+})
