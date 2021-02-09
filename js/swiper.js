d303a76856e543a388d4dcf3478d604b38ad577d
HannaHorbenko <annasgorbenko@gmail.com>
2021-02-09 12:56:56 +0200

    fixed

--- js/swiper.js
+++ js/swiper.js
@@ -31,30 +31,4 @@ let profileSlider = new Swiper('.profile-slider', {
   }
 });
 
-let portfolioSlider = new Swiper('.portfolio-slider', {
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev'
-  },
-  loop: true,
-  pagination: {
-    el: '.swiper-pagination',
-    dynamicBullets: true
-  },
-  slidesPerView: 3,
-  spaceBetween: 20,
-  autoHeight: true,
-  breakpoints: {
-    320: {
-      slidesPerView: 1,
-    },
-    480: {
-      slidesPerView: 2,
-    },
-    992: {
-      slidesPerView: 3,
-    }
-  }
-
-});
 
